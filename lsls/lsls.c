7f51e32808c4097f55d3eb1ddd8d8bc26b1fda37
McNultyPT <mcnultypt@gmail.com>
2019-06-07 10:26:50 -0700

    completing mvp

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <sys/stat.h>
+#include <string.h>
 
 /**
  * Main
@@ -7,14 +9,34 @@
 int main(int argc, char **argv)
 {
   // Parse command line
+  DIR *dir;
+  struct dirent *dirent;
+  struct stat buf;
 
   // Open directory
+  if (argc < 2) {
+    dir = opendir(".");
+  } else {
+    dir = opendir(argv[1]);
+  }
+
+  if (dir == NULL) {
+    return 1;
+  }
 
   // Repeatly read and print entries
+  char filepath[] = "./";
+
+  while((dirent = readdir(dir)) != NULL) {
+    char *copypath = strdup(filepath);
+    strcat(copypath, dirent->d_name);
+    stat(copypath, &buf);
+    printf("%lld bytes %s\n", buf.st_size, dirent->d_name);
+  }
 
   // Close directory
-
+  closedir(dir);
   return 0;
 }
 
-// initial push
+//  gcc -Wall -Wextra -o lsls lsls.c
